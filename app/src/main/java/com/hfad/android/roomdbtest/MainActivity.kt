b89ff9cf256947aab1a857c469550655fde562bf
peto40 <petrosmalkhasyan13@gmail.com>
2022-06-06 21:54:50 +0400

    last commit

--- app/src/main/java/com/hfad/android/roomdbtest/MainActivity.kt
+++ app/src/main/java/com/hfad/android/roomdbtest/MainActivity.kt
@@ -6,28 +6,50 @@ import android.os.Bundle
 import android.util.Log
 
 import com.hfad.android.roomdbtest.databinding.ActivityMainBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 const val KEY_COUNT = "key_count"
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        GlobalScope.launch(Dispatchers.IO) {
 
+            val userDao = UserDB.getInstance(this@MainActivity).userDao()
+            userDao.addUser(UserModel(0,"James","Rodrigue"))
+            userDao.addUser(UserModel(0,"Andy","Symon"))
+        }
 
+        binding.button.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                val user = UserDB.getInstance(this@MainActivity).userDao()
+                    .getUserBySurname("Symon").get(0)
+
+                binding.title.setText(user.surname)
+            }
+        }
+
+    }
+
+    fun getPref(){
         val prefs = getSharedPreferences("new_pref_file", Context.MODE_PRIVATE)
-       // val defaultPrefs = getPreferences(Context.MODE_PRIVATE)
+        // val defaultPrefs = getPreferences(Context.MODE_PRIVATE)
         var counter = prefs.getInt(KEY_COUNT, 0)
 
         binding.title.text = counter.toString()
 
         Log.d("dsadsad", "asdasdalnsdbuh")
-        Log.d("dsadsad", "asdasdalnsdbuh")
-        Log.d("dsadsad", "asdasdalnsdbuh")
+
         binding.button.setOnClickListener {
             counter++
 
--- app/src/main/java/com/hfad/android/roomdbtest/UserDao.kt
+++ app/src/main/java/com/hfad/android/roomdbtest/UserDao.kt
@@ -5,31 +5,34 @@ import androidx.room.*
 @Dao
 interface UserDao {
 
-    @Query("SELECT * FROM user")
-    fun getAllUsers(user: List<UserModel>)
-
-    @Query("SELECT * FROM user LIMIT 1")
-    fun getFirstElement() : UserModel
+//    @Query("SELECT * FROM user")
+//    fun getAllUsers(user: List<UserModel>)
+//
+//    @Query("SELECT * FROM user LIMIT 1")
+//    fun getFirstElement() : UserModel
+//
+//    @Query("SELECT * FROM user WHERE surname = :paramSurname")
+//    fun getUserBySurname(paramSurname: String): List<UserModel>
+//
+//    @Query("SELECT * FROM user WHERE surname LIKE :paramSurname")
+//    fun getUserBySurnameLike(paramSurname: String): List<UserModel>
+//
+//    @Query("SELECT * FROM user ")
 
     @Query("SELECT * FROM user WHERE surname = :paramSurname")
-    fun getUserBySurname(paramSurname: String): List<UserModel>
-
-    @Query("SELECT * FROM user WHERE surname LIKE :paramSurname")
-    fun getUserBySurnameLike(paramSurname: String): List<UserModel>
-
-    @Query("SELECT * FROM user ")
+    fun getUserBySurname(paramSurname: String) : List<UserModel>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addUser(userModel: UserModel)
-
-    @Insert
-    fun addUser(user: List<UserModel>)
-
-    @Delete
-    fun deleteUser(user: UserModel)
-
-    @Update
-    fun update(user: UserModel)
+//
+//    @Insert
+//    fun addUsers(users: List<UserModel>)
+//
+//    @Delete
+//    fun deleteUser(user: UserModel)
+//
+//    @Update
+//    fun update(user: UserModel)
 
 
 }
